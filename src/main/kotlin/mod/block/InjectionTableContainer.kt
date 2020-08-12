2f2ba94f0ab8cfe81f59c2084e569b5fe3e320e5
SOTA Gummy <64402144+SOTAGummy@users.noreply.github.com>
2020-08-12 10:43:52 +0900

    mod source

--- src/main/kotlin/mod/block/InjectionTableContainer.kt
+++ src/main/kotlin/mod/block/InjectionTableContainer.kt
@@ -14,7 +14,7 @@ import net.minecraftforge.items.SlotItemHandler
 class InjectionTableContainer(playerInv: IInventory, val te: TileEntityInjectionTable) : Container() {
 	init {
 		var inventory: IItemHandler? = te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH)
-		addSlotToContainer(object: SlotItemHandler(inventory, 0, 50, 20){
+		this.addSlotToContainer(object: SlotItemHandler(inventory, 0, 50, 20){
 			override fun onSlotChanged() {
 				te.markDirty()
 			}
@@ -27,12 +27,12 @@ class InjectionTableContainer(playerInv: IInventory, val te: TileEntityInjection
 
 		for (i in 0..2) {
 			for (j in 0..8) {
-				addSlotToContainer(Slot(playerInv, i * 9 + j + 9, 8 + j * 18, 84 + i * 18))
+				this.addSlotToContainer(Slot(playerInv, i * 9 + j + 9, 8 + j * 18, 84 + i * 18))
 			}
 		}
 
 		repeat(9) {
-			addSlotToContainer(Slot(playerInv, it, 8 + it * 18, 142))
+			this.addSlotToContainer(Slot(playerInv, it, 8 + it * 18, 142))
 		}
 	}
 
@@ -41,29 +41,36 @@ class InjectionTableContainer(playerInv: IInventory, val te: TileEntityInjection
 	}
 
 	override fun transferStackInSlot(playerIn: EntityPlayer, index: Int): ItemStack {
-		var itemstack = ItemStack.EMPTY
-		val slot = inventorySlots[index]
+		var itemstack = ItemStack.EMPTY;
+		var slot = inventorySlots[index];
+
 		if (slot != null && slot.hasStack) {
-			val itemstack1 = slot.stack
-			itemstack = itemstack1.copy()
-			val containerSlots = inventorySlots.size - playerIn.inventory.mainInventory.size
+			var itemstack1 = slot.stack;
+			itemstack = itemstack1.copy();
+
+			var containerSlots = inventorySlots.size - playerIn.inventory.mainInventory.size;
+
 			if (index < containerSlots) {
-				if (!mergeItemStack(itemstack1, containerSlots, inventorySlots.size, true)) {
-					return ItemStack.EMPTY
+				if (!this.mergeItemStack(itemstack1, containerSlots, inventorySlots.size, true)) {
+					return ItemStack.EMPTY;
 				}
-			} else if (!mergeItemStack(itemstack1, 0, containerSlots, false)) {
-				return ItemStack.EMPTY
+			} else if (!this.mergeItemStack(itemstack1, 0, containerSlots, false)) {
+				return ItemStack.EMPTY;
 			}
+
 			if (itemstack1.count == 0) {
-				slot.putStack(ItemStack.EMPTY)
+				slot.putStack(ItemStack.EMPTY);
 			} else {
-				slot.onSlotChanged()
+				slot.onSlotChanged();
 			}
+
 			if (itemstack1.count == itemstack.count) {
-				return ItemStack.EMPTY
+				return ItemStack.EMPTY;
 			}
-			slot.onTake(playerIn, itemstack1)
+
+			slot.onTake(playerIn, itemstack1);
 		}
-		return itemstack
+
+		return itemstack;
 	}
 }
