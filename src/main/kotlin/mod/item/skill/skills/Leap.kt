bffe420169d5f94e31ec5b6fe7fd60aff8e26acd
SOTA Gummy <64402144+SOTAGummy@users.noreply.github.com>
2020-08-17 03:36:00 +0900

    mod source

--- src/main/kotlin/mod/item/skill/skills/Leap.kt
+++ src/main/kotlin/mod/item/skill/skills/Leap.kt
@@ -5,12 +5,17 @@ import mod.item.skill.SkillRarity
 import mod.util.StatusUtil
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.util.EnumHand
+import net.minecraft.util.math.MathHelper
 import net.minecraft.world.World
 
-object Leap: ItemSkill("leap", 0, SkillRarity.EPIC){
+
+object Leap: ItemSkill("leap", 25, SkillRarity.EPIC){
 	override fun skillFunction(world: World, player: EntityPlayer, handIn: EnumHand) {
 		if (StatusUtil().useMP(player, this.cost)) {
-			player.addVelocity(player.pitchYaw.x.toDouble() / 100, -player.pitchYaw.y.toDouble(), 1.0)
+			val vx = -MathHelper.sin(Math.toRadians(player.rotationYaw.toDouble()).toFloat()) * MathHelper.cos(Math.toRadians(player.rotationPitch.toDouble()).toFloat())
+			val vz = MathHelper.cos(Math.toRadians(player.rotationYaw.toDouble()).toFloat()) * MathHelper.cos(Math.toRadians(player.rotationPitch.toDouble()).toFloat())
+			val vy = -MathHelper.sin(Math.toRadians(player.rotationPitch.toDouble()).toFloat())
+			player.addVelocity(vx.toDouble(), vy.toDouble(), vz.toDouble())
 		}
 	}
 }
