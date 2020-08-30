cc682290d494373d0ce7bc523833e00bca0a7c07
SOTA Gummy <64402144+SOTAGummy@users.noreply.github.com>
2020-08-30 13:50:58 +0900

    mod source

--- src/main/kotlin/mod/enchantment/EnchantmentInteligence.kt
+++
@@ -1,8 +0,0 @@
-package mod.enchantment
-
-import net.minecraft.enchantment.Enchantment
-import net.minecraft.enchantment.EnumEnchantmentType
-import net.minecraft.inventory.EntityEquipmentSlot
-
-class EnchantmentInteligence: Enchantment(Enchantment.Rarity.RARE, EnumEnchantmentType.ARMOR, null){
-}
